5046588e470252d8fe3e289bcfb45ea7a82d26db
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-09 17:04:13 -0300

    Settings

---
+++ molecule_generation_VAE.py
@@ -0,0 +1,22 @@
+#!pip -q install rdkit-pypi==2021.9.4
+
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
+
+import ast
+
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+import keras
+from keras import layers
+from keras import ops
+
+import matplotlib.pyplot as plt
+from rdkit import Chem, RDLogger
+from rdkit.Chem import BondType
+from rdkit.Chem.Draw import MolsToGridImage
+
+RDLogger.DisableLog("rdApp.*")
