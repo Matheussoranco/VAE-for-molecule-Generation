da2b098c48e56ba06a99b48e3ea12ac17b044cf5
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-10 17:00:37 -0300

    Data import

--- molecule_generation_VAE.py
+++ molecule_generation_VAE.py
@@ -20,3 +20,12 @@ from rdkit.Chem import BondType
 from rdkit.Chem.Draw import MolsToGridImage
 
 RDLogger.DisableLog("rdApp.*")
+
+csv_path = keras.utils.get_file(
+    "250k_rndm_zinc_drugs_clean_3.csv",
+    "https://raw.githubusercontent.com/aspuru-guzik-group/chemical_vae/master/models/zinc_properties/250k_rndm_zinc_drugs_clean_3.csv",
+)
+
+df = pd.read_csv(csv_path)
+df["smiles"] = df["smiles"].apply(lambda s: s.replace("\n", ""))
+df.head()
