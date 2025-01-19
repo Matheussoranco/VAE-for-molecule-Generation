9e76630e4c62f14e3d96a42f7db119ec2170592c
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-19 17:05:33 -0300

    Hyperparaematers and molecule modelling

--- molecule_generation_VAE.py
+++ molecule_generation_VAE.py
@@ -29,3 +29,88 @@ csv_path = keras.utils.get_file(
 df = pd.read_csv(csv_path)
 df["smiles"] = df["smiles"].apply(lambda s: s.replace("\n", ""))
 df.head()
+
+---
+## Hyperparameters
+SMILE_CHARSET = '["C", "B", "F", "I", "H", "O", "N", "S", "P", "Cl", "Br"]'
+
+bond_mapping = {"SINGLE": 0, "DOUBLE": 1, "TRIPLE": 2, "AROMATIC": 3}
+bond_mapping.update(
+    {0: BondType.SINGLE, 1: BondType.DOUBLE, 2: BondType.TRIPLE, 3: BondType.AROMATIC}
+)
+SMILE_CHARSET = ast.literal_eval(SMILE_CHARSET)
+
+MAX_MOLSIZE = max(df["smiles"].str.len())
+SMILE_to_index = dict((c, i) for i, c in enumerate(SMILE_CHARSET))
+index_to_SMILE = dict((i, c) for i, c in enumerate(SMILE_CHARSET))
+atom_mapping = dict(SMILE_to_index)
+atom_mapping.update(index_to_SMILE)
+
+BATCH_SIZE = 100
+EPOCHS = 10
+
+VAE_LR = 5e-4
+NUM_ATOMS = 120  # Maximum number of atoms
+
+ATOM_DIM = len(SMILE_CHARSET)  # Number of atom types
+BOND_DIM = 4 + 1  # Number of bond types
+LATENT_DIM = 435  # Size of the latent space
+
+
+def smiles_to_graph(smiles):
+    molecule = Chem.MolFromSmiles(smiles)
+
+    # Initialize adjacency and feature tensor
+    adjacency = np.zeros((BOND_DIM, NUM_ATOMS, NUM_ATOMS), "float32")
+    features = np.zeros((NUM_ATOMS, ATOM_DIM), "float32")
+
+    for atom in molecule.GetAtoms():
+        i = atom.GetIdx()
+        atom_type = atom_mapping[atom.GetSymbol()]
+        features[i] = np.eye(ATOM_DIM)[atom_type]
+        # loop over one-hop neighbors
+        for neighbor in atom.GetNeighbors():
+            j = neighbor.GetIdx()
+            bond = molecule.GetBondBetweenAtoms(i, j)
+            bond_type_idx = bond_mapping[bond.GetBondType().name]
+            adjacency[bond_type_idx, [i, j], [j, i]] = 1
+
+
+    adjacency[-1, np.sum(adjacency, axis=0) == 0] = 1
+
+    features[np.where(np.sum(features, axis=1) == 0)[0], -1] = 1
+
+    return adjacency, features
+
+
+def graph_to_molecule(graph):
+    adjacency, features = graph
+
+    molecule = Chem.RWMol()
+
+    keep_idx = np.where(
+        (np.argmax(features, axis=1) != ATOM_DIM - 1)
+        & (np.sum(adjacency[:-1], axis=(0, 1)) != 0)
+    )[0]
+    features = features[keep_idx]
+    adjacency = adjacency[:, keep_idx, :][:, :, keep_idx]
+
+    for atom_type_idx in np.argmax(features, axis=1):
+        atom = Chem.Atom(atom_mapping[atom_type_idx])
+        _ = molecule.AddAtom(atom)
+
+
+    (bonds_ij, atoms_i, atoms_j) = np.where(np.triu(adjacency) == 1)
+    for bond_ij, atom_i, atom_j in zip(bonds_ij, atoms_i, atoms_j):
+        if atom_i == atom_j or bond_ij == BOND_DIM - 1:
+            continue
+        bond_type = bond_mapping[bond_ij]
+        molecule.AddBond(int(atom_i), int(atom_j), bond_type)
+
+    # Sanitize the molecule; for more information on sanitization, see
+    # https://www.rdkit.org/docs/RDKit_Book.html#molecular-sanitization
+    flag = Chem.SanitizeMol(molecule, catchErrors=True)
+    if flag != Chem.SanitizeFlags.SANITIZE_NONE:
+        return None
+
+    return molecule
