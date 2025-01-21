ec594b8dde168729dfd2a7946dfb28e44ee73fa2
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2025-01-21 19:51:28 -0300

    ConvLayer for relational Graph

--- molecule_generation_VAE.py
+++ molecule_generation_VAE.py
@@ -129,3 +129,59 @@ for idx in range(8000):
 adjacency_tensor = np.array(adjacency_tensor)
 feature_tensor = np.array(feature_tensor)
 qed_tensor = np.array(qed_tensor)
+
+class RelationalGraphConvLayer(keras.layers.Layer):
+    def __init__(
+        self,
+        units=128,
+        activation="relu",
+        use_bias=False,
+        kernel_initializer="glorot_uniform",
+        bias_initializer="zeros",
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+
+        self.units = units
+        self.activation = keras.activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = keras.initializers.get(kernel_initializer)
+        self.bias_initializer = keras.initializers.get(bias_initializer)
+        self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
+        self.bias_regularizer = keras.regularizers.get(bias_regularizer)
+
+    def build(self, input_shape):
+        bond_dim = input_shape[0][1]
+        atom_dim = input_shape[1][2]
+
+        self.kernel = self.add_weight(
+            shape=(bond_dim, atom_dim, self.units),
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            trainable=True,
+            name="W",
+            dtype="float32",
+        )
+
+        if self.use_bias:
+            self.bias = self.add_weight(
+                shape=(bond_dim, 1, self.units),
+                initializer=self.bias_initializer,
+                regularizer=self.bias_regularizer,
+                trainable=True,
+                name="b",
+                dtype="float32",
+            )
+
+        self.built = True
+
+    def call(self, inputs, training=False):
+        adjacency, features = inputs
+        x = ops.matmul(adjacency, features[:, None])
+        x = ops.matmul(x, self.kernel)
+        if self.use_bias:
+            x += self.bias
+        x_reduced = ops.sum(x, axis=1)
+        return self.activation(x_reduced)
